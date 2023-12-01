409391fc4059f71b63feae41d95e5044b226272d
MatthewShindel <matthew@shindel.com>
2023-11-30 20:31:46 -0500

    add game-test.js and tests, add start() in game.js, run game in index.js

--- src/game.js
+++ src/game.js
@@ -2,6 +2,11 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const { createCard } = require('../src/card');
+const { createDeck , countCards} = require('../src/deck');
+const { prototypeData } = require('../src/data');
+const { createRound, takeTurn, calculatePercentCorrect, endRound} = require('../src/round');
+
 function printMessage(deck) {
   console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
   -----------------------------------------------------------------------`);
@@ -11,4 +16,14 @@ function printQuestion(round) {
   util.main(round);
 }
 
-module.exports = { printMessage, printQuestion };
+function start() {
+	let cards = prototypeData.slice(0, prototypeData.length);
+	let deck = createDeck(cards);
+	let round = createRound(deck);
+	printMessage(deck);
+	printQuestion(round);
+}
+
+module.exports = { printMessage, printQuestion, start};
+
+
